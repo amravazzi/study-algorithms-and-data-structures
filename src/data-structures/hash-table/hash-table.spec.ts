fe3d461e93a39711819be2aea4aade33794324c1
Andre Ravazzi <amravazzi@gmail.com>
2023-03-22 11:08:16 -0300

    feat: finish hash table implementation

--- src/data-structures/hash-table/hash-table.spec.ts
+++ src/data-structures/hash-table/hash-table.spec.ts
@@ -1,19 +1,55 @@
 import { HashTable } from "./hash-table";
 
 describe("hash table", () => {
-  it.todo("should set a key and a value in the hash table");
-  it.todo("should get a value from the hash table, given a key as argument");
+  it("should set a key and a value in the hash table", () => {
+    const key = "apple";
+    const value = 50;
+
+    const ht = new HashTable(20);
+    ht.set(key, value);
+
+    const hash = 4;
+    const tuple = ht["data"][hash];
+
+    expect(tuple[0][0]).toBe(key);
+    expect(tuple[0][1]).toBe(value);
+  });
+
+  it("should get a value from the hash table, given a key as argument", () => {
+    const key = "apple";
+    const value = 50;
+
+    const ht = new HashTable(20);
+    ht.set(key, value);
+    const returned_value = ht.get(key);
+
+    expect(returned_value).toBe(value);
+  });
+
   it("should hash a key idepontently", () => {
     const key = "test_test_123";
     const expected_hashed_hey = 5;
 
     const ht = new HashTable(20);
-    const hashed_key_1 = ht._hash(key);
-    const hashed_key_2 = ht._hash(key);
-    const hashed_key_3 = ht._hash(key);
+    const hashed_key_1 = ht["hash"](key);
+    const hashed_key_2 = ht["hash"](key);
+    const hashed_key_3 = ht["hash"](key);
 
     expect(hashed_key_1).toBe(expected_hashed_hey);
     expect(hashed_key_2).toBe(expected_hashed_hey);
     expect(hashed_key_3).toBe(expected_hashed_hey);
+  });
+
+  it("should return all the hash table keys", () => {
+    const ht = new HashTable(20);
+    ht.set("grapes", 14);
+    ht.set("apple", 25);
+    ht.set("oranges", 16);
+    ht.set("banana", 13);
+
+    const keys = ht.keys();
+
+    expect(keys).toHaveLength(4);
+    expect(keys).toEqual(["grapes", "apple", "oranges", "banana"]);
   });
 });
--- src/data-structures/hash-table/hash-table.ts
+++ src/data-structures/hash-table/hash-table.ts
@@ -1,14 +1,15 @@
 type hash = number;
-type bucket = [string, hash];
+type tuple<T> = [key: string, value: T];
+type bucket<T> = tuple<T>[];
 
 class HashTable {
-  private data: bucket[];
+  private data: bucket<any>[] = [];
 
   constructor(size: number) {
     this.data = new Array(size);
   }
 
-  _hash(key: string) {
+  private hash(key: string): hash {
     let hash = 0;
     for (let i = 0; i < key.length; i++) {
       hash = (hash + key.charCodeAt(i) * i) % this.data.length;
@@ -16,9 +17,36 @@ class HashTable {
     return hash;
   }
 
-  get(key: string) {}
+  get(key: string) {
+    const address = this.hash(key);
+    const bucket = this.data[address];
 
-  set(key: string, value: any) {}
+    if (bucket) {
+      for (let i = 0; i < bucket.length; i++) {
+        if (bucket[i][0] === key) return bucket[i][1];
+      }
+    }
+
+    return undefined;
+  }
+
+  set(key: string, value: any): void {
+    const address = this.hash(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+    this.data[address].push([key, value]);
+  }
+
+  keys(): tuple<any>[0][] {
+    const keys = [];
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        keys.push(this.data[i][0][0]);
+      }
+    }
+    return keys;
+  }
 }
 
 export { HashTable };
