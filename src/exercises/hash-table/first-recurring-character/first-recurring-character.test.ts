f9039d56a2ece5da21a225103f33d4aecd25c09e
Andre Ravazzi <amravazzi@gmail.com>
2023-03-23 10:51:33 -0300

    feat: finish hash table exercise

--- src/exercises/hash-table/first-recurring-character/first-recurring-character.test.ts
+++ src/exercises/hash-table/first-recurring-character/first-recurring-character.test.ts
@@ -1,24 +1,12 @@
-describe("First recurring character", () => {
-  it.todo(
-    "should return 2 given the array [2, 5, 5, 2, 3, 5, 1, 2, 4]",
-    () => {}
-  );
-  it.todo(
-    "should return 1 given the array [2, 1, 1, 2, 3, 5, 1, 2, 4]",
-    () => {}
-  );
-  it.todo("should return undefined given the array [2, 3, 4, 5]", () => {});
-  it.todo(
-    "should return 5 given the array [2, 5, 5, 2, 3, 5, 1, 2, 4]",
-    () => {}
-  );
+import { firstRecurringChar } from "./first-recurring-character";
 
+describe("First recurring character", () => {
   test.each([
-    [[2, 5, 5, 2, 3, 5, 1, 2, 4], 2],
+    [[2, 5, 1, 2, 3, 5, 1, 2, 4], 2],
     [[2, 1, 1, 2, 3, 5, 1, 2, 4], 1],
     [[2, 3, 4, 5], undefined],
-    [[2, 5, 5, 2, 3, 5, 1, 2, 4], 5]
-  ])('should return %i given the array %i', (arr, result) => {
-
+    [[2, 5, 5, 2, 3, 5, 1, 2, 4], 5],
+  ])("given the array %i, it should return %i", (arr, result) => {
+    expect(firstRecurringChar(arr)).toBe(result);
   });
 });
--- src/exercises/hash-table/first-recurring-character/first-recurring-character.ts
+++ src/exercises/hash-table/first-recurring-character/first-recurring-character.ts
@@ -1,11 +1,27 @@
 type returnType = number | undefined;
-
-export function firstRecurringChar(arr: number[]): number {
-  return 0;
+interface IMap<T> {
+  [key: string]: T;
 }
 
+// O(n)
+// W(n)
+export function firstRecurringChar(input: number[]): returnType {
+  let map: IMap<number> = {};
+  for (let i = 0; i < input.length; i++) {
+    console.log(map);
+    if (map[input[i]] !== undefined) {
+      return input[i];
+    }
+    map[input[i]] = i;
+  }
+  return undefined;
+}
+
+// O(n^2)
+// W(1)
 export function naiveFirstRecurringChar(arr: number[]): returnType {
   for (let i = 0; i < arr.length; i++) {
+    // i + 1 prevents looping through items already compared
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[i] === arr[j]) return arr[i];
     }
