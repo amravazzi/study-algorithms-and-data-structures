d95c01d9511d054e5ab09c750f729315e7878b8b
Andre Ravazzi <amravazzi@gmail.com>
2023-03-23 16:07:08 -0300

    feat: update linked lists methods and interfaces

--- src/data-structures/linked-lists/linked-lists.spec.ts
+++ src/data-structures/linked-lists/linked-lists.spec.ts
@@ -1,5 +1,106 @@
-import { LinkedList } from "./linked-lists";
+import { LinkedList, Node } from "./linked-lists";
 
 describe("linked lists", () => {
-  it("", () => {});
+  it("should initialize a ll with only one node with given value", () => {
+    const value = 10;
+    const ll = new LinkedList<number>(value);
+
+    expect(ll.length).toBe(1);
+    expect(ll.head).toEqual({ value, next: null });
+    expect(ll.tail).toEqual({ value, next: null });
+  });
+
+  it("should append the new value to the tail of the ll", () => {
+    const value1 = 10;
+    const value2 = 5;
+    const ll = new LinkedList<number>(value1);
+    const updated_ll = ll.append(value2);
+
+    expect(ll.length).toBe(2);
+    expect(ll.head).toEqual({
+      value: value1,
+      next: {
+        value: value2,
+        next: null,
+      },
+    });
+    expect(ll.tail).toEqual({ value: value2, next: null });
+    expect(updated_ll).not.toEqual({ value: value1, next: null });
+  });
+
+  it("should prepend the new value to the tail of the ll", () => {
+    const value1 = 5;
+    const value2 = 10;
+    const ll = new LinkedList<number>(value1);
+    const updated_ll = ll.prepend(value2);
+
+    expect(ll.length).toBe(2);
+    expect(ll.head).toEqual({
+      value: value2,
+      next: {
+        value: value1,
+        next: null,
+      },
+    });
+    expect(ll.tail).toEqual({ value: value1, next: null });
+    expect(updated_ll).not.toEqual({ value: value1, next: null });
+  });
+
+  it("should print the ll", () => {
+    const value1 = 5;
+    const value2 = 16;
+    const value3 = 1;
+    const ll = new LinkedList<number>(value1);
+    ll.append(value2);
+    ll.append(value3);
+
+    const printed_ll = ll.print();
+
+    expect(printed_ll.length).toBe(3);
+    expect(printed_ll).toEqual([value1, value2, value3]);
+  });
+
+  it("should insert a given value in a given index", () => {
+    const value1 = 5;
+    const value2 = 16;
+    const value3 = 1;
+
+    const index = 2;
+    const value4 = 13;
+
+    const ll = new LinkedList<number>(value1);
+    ll.append(value2);
+    ll.append(value3);
+    ll.insert(index, value4);
+    const printed_ll = ll.print();
+
+    expect(ll.length).toBe(4);
+    expect(printed_ll[index]).toBe(value4);
+    expect(printed_ll).toEqual([value1, value2, value4, value3]);
+  });
+
+  it("should return the node of a given index", () => {
+    const value1 = 5;
+    const value2 = 16;
+    const value3 = 1;
+    const value4 = 13;
+    const index = 2;
+
+    const ll = new LinkedList<number>(value1);
+    ll.append(value2);
+    ll.append(value3);
+    ll.append(value4);
+    const node = ll.lookup(index);
+
+    expect(node.value).toBe(value3);
+  });
+});
+
+describe("linked lists' node", () => {
+  it("should initialize an empty Node from given value", () => {
+    const value = 10;
+    const ll_node = new Node<number>(value);
+
+    expect(ll_node).toEqual({ value, next: null });
+  });
 });
---
+++ src/data-structures/linked-lists/linked-lists.ts
@@ -0,0 +1,105 @@
+type NodeValue<V> = V;
+
+type NodeNext<V> = INode<V> | null;
+
+interface INode<V> {
+  value: NodeValue<V>;
+  next: INode<V> | null;
+}
+
+interface ILinkedList<V> {
+  head: INode<V>;
+  tail: INode<V>;
+  length: number;
+  append(value: V): void;
+  prepend(value: V): LinkedList<V>;
+  insert(index: number, value: V): NodeValue<V>[];
+  lookup(index: number): INode<V>;
+}
+
+class LinkedList<V> implements ILinkedList<V> {
+  head: INode<V>;
+  tail: INode<V>;
+  length: number;
+
+  constructor(value: V) {
+    this.head = new Node(value);
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value: V): LinkedList<V> {
+    const newNode = new Node(value);
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value: V): LinkedList<V> {
+    const newNode = new Node(value);
+
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  print(): NodeValue<V>[] {
+    let currNode: NodeNext<V> = this.head;
+    let arr: NodeValue<V>[] = [];
+
+    while (currNode) {
+      arr = [...arr, currNode.value];
+      currNode = currNode.next;
+    }
+
+    return arr;
+  }
+
+  //
+  insert(index: number, value: V): NodeValue<V>[] {
+    if (index === 0) {
+      this.prepend(value);
+      return this.print();
+    }
+
+    if (index >= this.length) {
+      this.append(value);
+      return this.print();
+    }
+
+    const newNode = new Node(value);
+    let leaderNode = this.lookup(index - 1);
+
+    const holdingPointer = leaderNode.next;
+    leaderNode.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+
+    return this.print();
+  }
+
+  lookup(index: number): INode<V> {
+    let currNode: NodeNext<V> = this.head;
+    let counter = 0;
+    while (counter !== index && currNode.next) {
+      currNode = currNode.next;
+      counter++;
+    }
+    return currNode;
+  }
+}
+
+class Node<V> implements INode<V> {
+  value: V;
+  next: INode<V> | null;
+
+  constructor(value: V) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+export { LinkedList, Node };
